5eb1bf46b30ff768887eea1822b5d76628f805b7
thekimpeople <thekimpeople@gmail.com>
2025-06-01 17:10:30 +0300

    footer

--- components/casestudies.tsx
+++ components/casestudies.tsx
@@ -1,8 +1,10 @@
+"use client"
 import { ArrowRightIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import { truncateText } from '@/lib/utils';
+import { motion } from 'framer-motion';
 
 export default function CaseStudies() {
 
@@ -30,28 +32,45 @@ export default function CaseStudies() {
         }
     ];
 
-  return (
-    <div className='bg-[url("/images/redbg.jpg")] bg-cover bg-center pb-8'>
-        <div className='container mx-auto grid md:grid-cols-2 gap-6 px-6'>
-            <div className=''>
-                <h2 className='text-3xl font-bold text-yellow-300 mt-10'>Case Studies</h2>
-                <p className='text-white md:text-4xl mt-4 text-2xl'>Explore our case studies to see how we have helped businesses like yours achieve their goals.</p>
-                <div className='my-10 ml-0'>
+    return (
+        <div className='bg-[url("/images/redbg.jpg")] bg-cover bg-center pb-8'>
+            <div className='container mx-auto grid md:grid-cols-2 gap-6 px-6 mb-10'>
+                <div>
+                    <h2 className='text-3xl font-bold text-yellow-300 mt-10'>Case Studies</h2>
+                    <p className='text-white md:text-4xl mt-4 text-2xl'>
+                        Explore our case studies to see how we have helped businesses like yours achieve their goals.
+                    </p>
                 </div>
             </div>
+
+            <div className='container mx-auto grid md:grid-cols-3 gap-6 px-6 mb-10'>
+                {caseStudies.map((caseStudy, index) => (
+                    <motion.div
+                        key={caseStudy.id}
+                        className='bg-white p-6 shadow-lg hover:shadow-xl transition-shadow duration-300'
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5, delay: index * 0.2 }}
+                        viewport={{ once: true }}
+                    >
+                        <Image
+                            src={caseStudy.image}
+                            alt={caseStudy.title}
+                            width={500}
+                            height={300}
+                            className='w-full h-48 object-cover mb-4'
+                        />
+                        <h3 className='text-xl font-bold text-primary mt-10'>{caseStudy.title}</h3>
+                        <p className='text-gray-600 mt-4'>{truncateText(caseStudy.description, 150)}</p>
+                        <Link
+                            href={caseStudy.link}
+                            className='text-primary hover:underline flex items-center gap-2 mt-4'
+                        >
+                            Read More <ArrowRightIcon />
+                        </Link>
+                    </motion.div>
+                ))}
+            </div>
         </div>
-        <div className='container mx-auto grid md:grid-cols-3 gap-6 px-6 mb-10'>
-            {caseStudies.map((caseStudy) => (
-                <div key={caseStudy.id} className='bg-white p-6 shadow-lg hover:shadow-xl transition-shadow duration-300'>
-                    <Image src={caseStudy.image} alt={caseStudy.title} width={500} height={300} className='w-full h-48 object-cover mb-4' />
-                    <h3 className='text-xl font-bold text-primary mt-10'>{caseStudy.title}</h3>
-                    <p className='text-gray-600 mt-4'>{truncateText(caseStudy.description, 150)}</p>
-                    <Link href={caseStudy.link} className='text-primary hover:underline flex items-center gap-2 mt-4'>
-                        Read More <ArrowRightIcon/>
-                    </Link>
-                </div>
-            ))}
-        </div>
-    </div>
-  )
+    )
 }
